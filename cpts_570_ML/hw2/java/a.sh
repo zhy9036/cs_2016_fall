d958a2d4c9f6b1e1dea380c842244da2aaf8ce35
zhy9036 <zhangyang9036@gmail.com>
2016-10-02 00:17:01 -0700

    570 hw2

--- cpts_570_ML/hw2/java/a.sh
+++ cpts_570_ML/hw2/java/a.sh
@@ -1,2 +1,14 @@
-#!/bin/bash          
-          echo Hello World  
+#!/bin/bash
+
+c=0.0001  
+#java -classpath libsvm.jar svm_train -t 0 -c $c ./data/"fold1_sm_train.txt" ./modelFiles/"model1"        
+for i in {1..9}
+do
+	echo $c
+	for j in {0..9}
+	do
+		java -classpath libsvm.jar svm_train -t 0 -c $c ./data/"fold$(($j))_sm_train.txt" ./modelFiles/"model$(($i))$(($j))"
+		#pwd
+	done
+	c=$(echo "$c*10"|bc)
+done
