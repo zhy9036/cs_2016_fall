aa020a50da9e62e7a513d81bb3012a6e8c2d1977
zhy9036 <zhangyang9036@gmail.com>
2016-10-10 16:34:15 -0700

    sync

--- 534_ANN/hw4/hw4.m
+++ 534_ANN/hw4/hw4.m
@@ -1,6 +1,6 @@
-f = inline('800*log(40*(2*x)^3)<=x');
-ff = @(x) 800*log(40*(2*x)^3)<=x;
-y = fzero(ff,10.1111);
-%y = solve(800*log(40*(2*x)^3)-x<=0,x);
-%yy = solve(,x);
-disp(y);
+f1 = @(x) 800*log(40*(2*x)^3)-x;
+f2 = @(x) 800*log(40*(2*x)^6)-x;
+y1 = fzero(f1,110000);
+y2 = fzero(f2,110000);
+disp(y1);
+disp(y2);
